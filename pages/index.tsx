51eb23d7a856534e69d03ab75dc5e3c5a55fec7b
Joseph Chow <contact@josephchow.dev>
2023-02-16 12:53:14 -0500

    feat: fetch countries from api :bento:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,83 @@
 import type {NextPage} from 'next';
 import {Meta} from '@/components/Meta';
+import type {Country} from '@/types/countries';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+  Text,
+  Tooltip,
+  Flex,
+  Heading,
+} from '@chakra-ui/react';
+import Image from 'next/image';
 
-const Home: NextPage = () => {
+const URL = 'https://restcountries.com/v3.1/';
+
+export async function getStaticProps() {
+  const res = await fetch(`${URL}all`);
+  const data = await res.json();
+
+  return {
+    props: {
+      countries: data,
+    },
+  };
+}
+
+type Props = {
+  countries: Country[];
+};
+const Home: NextPage<Props> = ({countries}) => {
+  console.log('countries', countries);
   return (
     <>
       <Meta />
-      <h1>Hello World</h1>
+      <Grid
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          lg: 'repeat(2, 1fr)',
+          xl: 'repeat(3, 1fr)',
+          '2xl': 'repeat(4, 1fr)',
+        }}
+        gap="6"
+      >
+        {countries?.map((country) => (
+          <Card key={country.cca3}>
+            <CardHeader>
+              <Heading as="h2" size="lg">
+                {country.name.common}
+              </Heading>
+            </CardHeader>
+            <CardBody as={Flex} justifyContent="center">
+              <Tooltip label={country.flags.alt || 'No data 😔'}>
+                <Image
+                  aria-label="Country flag"
+                  src={country.flags.svg}
+                  alt={country.flags.alt || 'No data 😔'}
+                  width={300}
+                  height={200}
+                  style={{width: 'auto', height: 'auto'}}
+                  priority
+                />
+              </Tooltip>
+            </CardBody>
+            <CardFooter as={Flex} direction="column" gap="2">
+              <Text>
+                <Text as="strong">Population:</Text> {country.population}
+              </Text>
+              <Text>
+                <Text as="strong">Region:</Text> {country.region}
+              </Text>
+              <Text>
+                <Text as="strong">Capital:</Text> {country.capital}
+              </Text>
+            </CardFooter>
+          </Card>
+        ))}
+      </Grid>
     </>
   );
 };
---
+++ types/countries.ts
@@ -0,0 +1,89 @@
+export type Country = {
+  altSpellings?: string[];
+  area: number;
+  borders?: string[];
+  capital: string[];
+  capitalInfo: {
+    latlng: number[];
+  };
+  car: {
+    side: string;
+    signs: string[];
+  };
+  cca2: string;
+  cca3: string;
+  ccn3: string;
+  cioc: string;
+  coatOfArms: {
+    png: string;
+    svg: string;
+  };
+  continents: string[];
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  demonym: {
+    eng: {
+      f: string;
+      m: string;
+    };
+    fra: {
+      f: string;
+      m: string;
+    };
+  };
+  fifa: string;
+  flag: string;
+  flags: {
+    png: string;
+    svg: string;
+    alt?: string;
+  };
+  gini: {
+    [key: string]: number;
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  independent: boolean;
+  landlocked: boolean;
+  languages: {
+    [key: string]: string;
+  };
+  latlng: number[];
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  population: number;
+  postalCode: {
+    format: string;
+    regex: string;
+  };
+  region: string;
+  startOfWeek: string;
+  subregion: string;
+  timezones: string[];
+  tld: string[];
+  translations: {
+    [key: string]: {
+      official: string;
+      common: string;
+    }[];
+  };
+  unMember: boolean;
+};
