172cd3dd9553daffaa7f772c449281d432d7e094
Joseph Chow <contact@josephchow.dev>
2023-04-25 16:27:07 -0400

    chore: change to using next app directory and shadcn ui :building_construction:

--- utils/settings/animations.ts
+++
@@ -1,5 +0,0 @@
-export const enterExitSpin = {
-  initial: {opacity: 0, scale: 0.5, x: 0, rotate: -180},
-  animate: {opacity: 1, scale: 1, x: 0, rotate: 0},
-  exit: {opacity: 0, scale: 0.5, x: 0, rotate: 180},
-};
--- utils/settings/constants.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-
-/* Will refactor this later */
-
-export const URL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:${process.env.PORT}/api/countries/`
-    : `${process.env.URL}/api/countries/`;
